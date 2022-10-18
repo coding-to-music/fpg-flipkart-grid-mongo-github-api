83faf7320bba8c54b9cdb0603f3744dac6b0bba7
coding-to-music <connors.tom@gmail.com>
2022-10-18 21:47:05 +0000

    Build

--- index.js
+++ index.js
@@ -8,98 +8,101 @@
 //     result(link);
 // }
 
-const express = require('express')
-const axios = require('axios')
-const dotenv= require('dotenv')
+const express = require("express");
+const axios = require("axios");
+const dotenv = require("dotenv");
 // const getBaseData = require("./scrappers/getBaseData");
-const scan = require('./scrappers/initiateScan');
+const scan = require("./scrappers/initiateScan");
 const { Octokit } = require("octokit");
 const mongoose = require("mongoose");
 const { Package } = require("./models/package");
 const { Owner } = require("./models/owner");
 
-dotenv.config()
+dotenv.config();
 
-const PORT=process.env.PORT || 3000
-const app = express()
+const PORT = process.env.PORT || 3000;
+const app = express();
 
 var allowCrossDomain = function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
 };
 
 app.use(express.json());
 app.use(allowCrossDomain);
 
-app.get('/', async (req,res)=> {
+app.get("/", async (req, res) => {
   let { url } = req.query;
 
   const octokit = new Octokit({
-    auth: process.env.TOKEN
+    auth: process.env.OCTOKIT_TOKEN,
   });
 
-  let [, owner, repo ] = url.match(/https:\/\/github.com\/(.*)\/(.*)(\/|)/);
-  let package = owner + '/' + repo;
+  let [, owner, repo] = url.match(/https:\/\/github.com\/(.*)\/(.*)(\/|)/);
+  let package = owner + "/" + repo;
   console.log(package);
-  const data = await Package.find({package});
+  const data = await Package.find({ package });
   console.log(data);
 
   if (data.length) {
     console.log(data);
     if (data[0].codeQLResult === undefined) {
-        res.status(200).send("scan in progress");
-        return;
+      res.status(200).send("scan in progress");
+      return;
     }
     const userInfo = (await octokit.request(`GET /users/${owner}`)).data;
-    res.status(200).send({data: data[0], owner: userInfo});
+    res.status(200).send({ data: data[0], owner: userInfo });
     return;
   } else {
     res.status(404).send();
     return;
   }
-})
+});
 
-app.post('/scan', async (req, res) => {
-    let {url, type} = req.body;
-    let [, owner, repo ] = url.match(/https:\/\/github.com\/(.*)\/(.*)(\/|)/);
-    let package = owner + "/" + repo;
-    const data = await Package.find({package});
+app.post("/scan", async (req, res) => {
+  let { url, type } = req.body;
+  let [, owner, repo] = url.match(/https:\/\/github.com\/(.*)\/(.*)(\/|)/);
+  let package = owner + "/" + repo;
+  const data = await Package.find({ package });
 
-    if (data.length) {
-        if (data[0].codeQLResult === undefined) {
-            res.status(200).send("scanning already in progress");
-            return;
-        }
-        else {
-            res.status(200).send({data: data[0]});
-            return;
-        }
+  if (data.length) {
+    if (data[0].codeQLResult === undefined) {
+      res.status(200).send("scanning already in progress");
+      return;
     } else {
-        const ownerData = await Owner.find({owner});
-        console.log(ownerData);
-        if (ownerData.length && ownerData[0].flagged !== undefined && ownerData[0].flagged) {
-            const pkg = new Package({
-                package,
-                owner,
-                codeQLResult: true,
-                reason: "owner already flagged"
-            });
-            await pkg.save();
-            res.status(200).send("scanning done");
-            return;
-        }
+      res.status(200).send({ data: data[0] });
+      return;
     }
+  } else {
+    const ownerData = await Owner.find({ owner });
+    console.log(ownerData);
+    if (
+      ownerData.length &&
+      ownerData[0].flagged !== undefined &&
+      ownerData[0].flagged
+    ) {
+      const pkg = new Package({
+        package,
+        owner,
+        codeQLResult: true,
+        reason: "owner already flagged",
+      });
+      await pkg.save();
+      res.status(200).send("scanning done");
+      return;
+    }
+  }
 
-    scan(owner, repo, type);
-    
-    res.status(200).send("scanning initialized");
-})
+  scan(owner, repo, type);
+
+  res.status(200).send("scanning initialized");
+});
 
 mongoose
-  .connect(process.env.MONGODB_URL)
-  .then(() => { 
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("connected to MongoDB");
     app.listen(PORT);
     console.log(`listening at port ${PORT}`);
